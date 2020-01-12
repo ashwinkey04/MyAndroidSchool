0a146d0adb909ded72f46bab0db988bd6ce31c7f
Ashwin R <ashwinkey04@gmail.com>
2020-01-12 21:55:01 +0530

    Add NavDrawer App

---
+++ NavDrawer/app/src/main/java/com/example/navdrawer/FaqViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.navdrawer
+
+import androidx.lifecycle.ViewModel
+
+class FaqViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ NavDrawer/app/src/main/java/com/example/navdrawer/faq.kt
@@ -0,0 +1,32 @@
+package com.example.navdrawer
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class faq : Fragment() {
+
+    companion object {
+        fun newInstance() = faq()
+    }
+
+    private lateinit var viewModel: FaqViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.faq_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(FaqViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
